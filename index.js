caf53d8d824bcebef1126afd43112cde343da12c
Ivan2007 <ivanhlibov2007@gmail.com>
2022-01-24 11:26:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const age = 100;
+
+if (age >= 55 && age  <=99){
+    console.log("Получилось");
+}else{
+    console.log(" Не получилось");
+}
+
+
+const age = 55;
+let name = 'Ребенок'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
